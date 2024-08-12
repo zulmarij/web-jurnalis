7a8ef4bcdef1a4fca3e401f931784cb9b7c3d93d
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2024-08-12 19:58:06 +0700

    fix route_path curator

--- config/curator.php
+++ config/curator.php
@@ -28,8 +28,7 @@ return [
     'glide' => [
         'server' => \Awcodes\Curator\Glide\DefaultServerFactory::class,
         'fallbacks' => [],
-        // 'route_path' => 'curator',
-        'route_path' => env('APP_URL'),
+        'route_path' => 'curator',
     ],
     'image_crop_aspect_ratio' => null,
     'image_resize_mode' => null,
