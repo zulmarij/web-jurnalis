be43d45045b5283cffbc35ba482911087b72e2a0
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2024-08-28 19:47:44 +0700

    update

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,10 +1,10 @@
 <section class="py-4">
     <x-posts.featured-post :post="$this->featuredPost" />
     <div class="max-w-screen-xl mx-auto px-4" >
-        <x-posts.featured-posts title="Berita" :posts="$this->beritaPosts" class="mb-4" />
-        <x-posts.featured-posts title="Edukasi" :posts="$this->edukasiPosts" class="mb-4" />
-        <x-posts.featured-posts title="Inspirasi" :posts="$this->inspirasiPosts" class="mb-4" />
-        <x-posts.featured-posts title="Inovasi" :posts="$this->inovasiPosts" class="mb-4" />
-        <x-posts.featured-posts title="Hiburan" :posts="$this->hiburanPosts" class="mb-4" />
+        <x-posts.featured-posts title="Berita" :posts="$this->beritaPosts" class="my-4" />
+        <x-posts.featured-posts title="Edukasi" :posts="$this->edukasiPosts" class="my-4" />
+        <x-posts.featured-posts title="Inspirasi" :posts="$this->inspirasiPosts" class="my-4" />
+        <x-posts.featured-posts title="Inovasi" :posts="$this->inovasiPosts" class="my-4" />
+        <x-posts.featured-posts title="Hiburan" :posts="$this->hiburanPosts" class="my-4" />
     </div>
 </section>
