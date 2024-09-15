05447931f7c008f5138f0f359cb2378577f5aaec
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2024-09-15 19:03:20 +0700

    updat

--- resources/views/components/posts/section-1.blade.php
+++ resources/views/components/posts/section-1.blade.php
@@ -14,7 +14,7 @@
                     class="bg-purple-100 text-purple-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-purple-200 dark:text-purple-900">
                     {{ $post->firstCategory()->name }}</span>
             </a>
-            <h2 class="my-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white line-clamp-2">
+            <h2 class="my-2 text-2xl font-bold  text-gray-900 dark:text-white line-clamp-2">
                 <a href="{{ route('post.show', ['slug' => $post->slug]) }}">{!! $post->title !!}</a>
             </h2>
             <p class="mb-4 font-light text-gray-900 dark:text-gray-400 line-clamp-3">
